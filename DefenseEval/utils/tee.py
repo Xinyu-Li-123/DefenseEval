62feb46ec7bc219595c84f5e37c6a9a58129f8cc
Xinyu Li <xl3665@nyu.edu>
2023-04-15 15:16:04 -0600

    auto log using tee

--- DefenseEval/utils/tee.py
+++ DefenseEval/utils/tee.py
@@ -1,34 +1,7 @@
-from pprint import pprint 
 import sys
 import traceback
 
 
-def print_pretty(obj):
-    pprint(obj)
-
-def print_block(string, end="\n"):
-    hash_num = 45
-    print("\n\n")
-    print("#" * hash_num)
-
-    mid_hash_num = min((hash_num - len(string) - 2) // 2, 3)
-    mid_hash = "#" * mid_hash_num
-    mid_space = " " * ((hash_num - 2 * mid_hash_num - len(string))//2)
-    print("{}{}{}{}{}".format(
-        mid_hash, mid_space, " "*len(string), mid_space, mid_hash
-    ))
-    print("{}{}{}{}{}".format(
-        mid_hash, mid_space, string, mid_space, mid_hash
-    ))
-    print("{}{}{}{}{}".format(
-        mid_hash, mid_space, " "*len(string), mid_space, mid_hash
-    ))
-    print("#" * hash_num)
-    print()
-
-
-# Both `DuplicateWriter` and `Tee` are contributed to the following repo:
-#       https://github.com/netsharecmu/NetShare/blob/0ade9916d27307e63a31d17afcbcb9785c14b9f0/netshare/utils/tee.py
 class DuplicateWriter(object):
     def __init__(self, file_objects):
         self._file_objects = file_objects
